877030c81c1aac9c2f0a58126142ce977b58861c
Nazirjon0125 <tukhtamirzaevnazirjon96@gmail.com>
2025-02-19 18:34:38 +0900

    fix: completed server and views

--- server.js
+++ server.js
@@ -15,13 +15,24 @@ app.set("views", "views");
 app.set("view engine", "ejs"); // EJS da backend orqali frontedni yasaymiz
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1>HELLO WORLD by John</h1>`);
+// POST malumotni olib kelib datebase ga yozadi
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// get datebas dan malumotni uqish uchun
+app.get("/", function (req, res) {
+  resizeBy.render("harid");
 });
+
+// app.get("/hello", function (req, res) {
+//   res.end(`<h1>HELLO WORLD by John</h1>`);
+// });
+
+// app.get("/gift", function (req, res) {
+//   res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// });
 
 const server = http.createServer(app);
 let PORT = 3000;
